cb9fe6f34e6148b791d261d7dbf25ef844f826ea
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2019-10-18 17:44:44 -0400

    Create Theme data type and Themes assembly

---
+++ Assets/Code/Themes/Theme.cs
@@ -0,0 +1,98 @@
+﻿using UnityEngine;
+
+namespace MinimalMiner.Themes
+{
+    public struct Theme
+    {
+        #region Fields
+
+        #region UI text
+        /// <summary>
+        /// UI - Text - Primary heading
+        /// </summary>
+        public Color32 text_primaryHead;
+
+        /// <summary>
+        /// UI - Text - Secondary heading
+        /// </summary>
+        public Color32 text_secondaryHead;
+
+        /// <summary>
+        /// UI - Text - Body text
+        /// </summary>
+        public Color32 text_body;
+
+        #endregion
+
+        #region Button colors
+
+        /// <summary>
+        /// UI - Button - Normal button color
+        /// </summary>
+        public Color32 button_normal;
+
+        /// <summary>
+        /// UI - Button - Color on mouse hover
+        /// </summary>
+        public Color32 button_hover;
+
+        /// <summary>
+        /// UI - Button - Color on mouse click
+        /// </summary>
+        public Color32 button_active;
+
+        /// <summary>
+        /// UI - Button - Color on button selected
+        /// </summary>
+        public Color32 button_focus;
+
+        /// <summary>
+        /// UI - Button - Disabled button color
+        /// </summary>
+        public Color32 button_disabled;
+
+        #endregion
+
+        #region Images
+
+        /// <summary>
+        /// Image - Background image used during normal game-play
+        /// </summary>
+        public Sprite img_backgroundNormal;
+
+        /// <summary>
+        /// Image - Color used when no background image present (applies to camera clear color)
+        /// </summary>
+        public Color32 img_backgroundColor;
+
+        #endregion
+
+        #endregion
+
+        #region Constructors
+
+        /// <summary>
+        /// Creates a basic theme
+        /// </summary>
+        /// <param name="fore">UI Foreground color</param>
+        /// <param name="back">UI Background color</param>
+        /// <param name="bkgd">World background</param>
+        public Theme(Color32 fore, Color32 back, Sprite bkgd)
+        {
+            text_primaryHead = fore;
+            text_secondaryHead = fore;
+            text_body = fore;
+
+            img_backgroundNormal = bkgd;
+            img_backgroundColor = back;
+
+            button_normal = back;
+            button_hover = back;
+            button_active = back;
+            button_focus = back;
+            button_disabled = back;
+        }
+
+        #endregion
+    }
+}
