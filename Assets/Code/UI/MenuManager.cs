9bbac2d1608bfbf7938246b521574c8a9dba41f9
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2019-10-18 20:04:01 -0400

    transferred some menumanager code over to new menumanager

--- Assets/Code/UI/MenuManager.cs
+++ Assets/Code/UI/MenuManager.cs
@@ -1,10 +1,49 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 using MinimalMiner.Util;
+using MinimalMiner.Themes;
 
 namespace MinimalMiner.UI
 {
+    /// <summary>
+    /// Handles in-game menu interfaces
+    /// </summary>
     public class MenuManager : MonoBehaviour
     {
+        // Canvases
+        [SerializeField] private GameObject canvasMain;
+        [SerializeField] private GameObject canvasSettings;
+        [SerializeField] private GameObject canvasPlay;
+        [SerializeField] private GameObject canvasPause;
+        [SerializeField] private GameObject canvasDeath;
+
+        // Collections of UI elements that need theme updates
+        private TextMeshProUGUI[] mainText;
+        private TextMeshProUGUI[] settingsText;
+        private TextMeshProUGUI[] playText;
+        private TextMeshProUGUI[] pauseText;
+        private TextMeshProUGUI[] deathText;
+        private Button[] mainButtons;
+        private Button[] settingsButtons;
+        private Button[] playButtons;
+        private Button[] pauseButtons;
+        private Button[] deathButtons;
+        private TMP_Dropdown[] settingsDropdowns;
+
+        /// <summary>
+        /// Start is called before the first frame update
+        /// </summary>
+        void Start()
+        {
+            canvasSettings.SetActive(false);
+            canvasPlay.SetActive(false);
+            canvasPause.SetActive(false);
+            canvasDeath.SetActive(false);
+            canvasMain.SetActive(true);
+            GrabUIElements();
+        }
+
         private void OnEnable()
         {
             EventManager.onUpdateTheme += UpdateTheme;
@@ -13,6 +52,26 @@ namespace MinimalMiner.UI
         private void OnDisable()
         {
             EventManager.onUpdateTheme -= UpdateTheme;
+        }
+
+        /// <summary>
+        /// Grabs various UI elements for theme updating
+        /// </summary>
+        private void GrabUIElements()
+        {
+            mainText = canvasMain.GetComponentsInChildren<TextMeshProUGUI>();
+            settingsText = canvasSettings.GetComponentsInChildren<TextMeshProUGUI>();
+            playText = canvasPlay.GetComponentsInChildren<TextMeshProUGUI>();
+            pauseText = canvasPause.GetComponentsInChildren<TextMeshProUGUI>();
+            deathText = canvasDeath.GetComponentsInChildren<TextMeshProUGUI>();
+
+            mainButtons = canvasMain.GetComponentsInChildren<Button>();
+            settingsButtons = canvasSettings.GetComponentsInChildren<Button>();
+            playButtons = canvasPlay.GetComponentsInChildren<Button>();
+            pauseButtons = canvasPause.GetComponentsInChildren<Button>();
+            deathButtons = canvasDeath.GetComponentsInChildren<Button>();
+
+            settingsDropdowns = canvasSettings.GetComponentsInChildren<TMP_Dropdown>();
         }
 
         public void UpdateTheme(int themeIndex)
--- Assets/Code/Util/EventManager.cs
+++ Assets/Code/Util/EventManager.cs
@@ -3,14 +3,41 @@ using UnityEngine;
 
 namespace MinimalMiner.Util
 {
+    /// <summary>
+    /// Represents the different game states (menus, paused vs gameplay, etc.)
+    /// </summary>
+    public enum GameState
+    {
+        main,
+        settings,
+        play,
+        pause,
+        death
+    }
+
+    /// <summary>
+    /// <para>Handles incoming events from UI for other managers to subscribe to</para>
+    /// Master GameState is tracked here
+    /// </summary>
     public class EventManager : MonoBehaviour
     {
         public delegate void OnUpdateTheme(int themeIndex);
         public static event OnUpdateTheme onUpdateTheme;
 
-        private void Start()
+        public delegate void OnUpdateGameState(GameState newState, GameState prevState);
+        public static event OnUpdateGameState onUpdateGameState;
+
+        private GameState currState;
+
+        public void UpdateGameState(GameState newState)
         {
-            onUpdateTheme(0);
+            onUpdateGameState(newState, currState);
+            currState = newState;
+        }
+
+        public void UpdateTheme(int themeIndex)
+        {
+            onUpdateTheme(themeIndex);
         }
     }
 }
