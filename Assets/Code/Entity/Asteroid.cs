837b8e369ed07afd325f22fe0f77b80e0617352c
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2019-10-19 00:28:13 -0400

    began working on asteroids and entities
    Asteroids will mainly use autoproperties, because there may be multiple properties that get changed at the same time in a single method

---
+++ Assets/Code/Entity/Asteroid.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MinimalMiner.Util;
+
+namespace MinimalMiner.Entity
+{
+    /// <summary>
+    /// Defines an asteroid gameobject in the scene
+    /// </summary>
+    public class Asteroid : MonoBehaviour
+    {
+        /// <summary>
+        /// The type that the asteroid is
+        /// </summary>
+        public AsteroidType Type
+        {
+            get; private set;
+        }
+
+        /// <summary>
+        /// The position of the asteroid
+        /// </summary>
+        public Vector3 Pos
+        {
+            get; private set;
+        }
+
+        /// <summary>
+        /// The velocity of the asteroid
+        /// </summary>
+        public Vector3 Vel
+        {
+            get; private set;
+        }
+
+        // Needed for tracking the current state of the asteroid
+        private GameState currState;
+
+        private void Start()
+        {
+            EventManager.onUpdateGameState += UpdateGameState;
+        }
+
+        /// <summary>
+        /// Used when first spawning an asteroid
+        /// </summary>
+        /// <param name="type">The type that the asteroid should be</param>
+        public void Setup(AsteroidType type)
+        {
+            Type = type;
+        }
+
+        private void UpdateGameState(GameState newState, GameState prevState)
+        {
+            currState = newState;
+        }
+    }
+}
---
+++ Assets/Code/Util/EntityTypes.cs
@@ -0,0 +1,10 @@
+﻿namespace MinimalMiner.Util
+{
+    /// <summary>
+    /// Defines the type that an asteroid is
+    /// </summary>
+    public enum AsteroidType
+    {
+        general
+    }
+}
