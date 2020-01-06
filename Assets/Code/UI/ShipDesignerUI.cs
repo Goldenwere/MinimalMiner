c7a809f4c461a354782cee9b50ae34d42574478e
Devin Jolly (Lightling) <xxlightlingxx@gmail.com>
2020-01-06 15:39:47 -0500

    handled weapon changes

--- Assets/Code/UI/ShipDesignerUI.cs
+++ Assets/Code/UI/ShipDesignerUI.cs
@@ -3,12 +3,16 @@
 using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 namespace MinimalMiner.UI
 {
     public class ShipDesignerUI : MonoBehaviour
     {
+        [SerializeField] private TMP_Dropdown weaponSelectionDropdown;
+        [SerializeField] private Slider[] individualWeaponSliders;
+        [SerializeField] private TMP_Dropdown[] individualWeaponDropdowns;
         [SerializeField] private string[] valueIndicatorTitles;
         [SerializeField] private TextMeshProUGUI[] valueIndicatorTexts;
         private Dictionary<string, TextMeshProUGUI> valueIndicators;
@@ -51,6 +55,33 @@ namespace MinimalMiner.UI
         {
             if (valueIndicators.ContainsKey(indicator))
                 valueIndicators[indicator].text = Math.Round(value, 2).ToString();
+        }
+
+        /// <summary>
+        /// Called when the slider for weapon count changes - redfines the options for the weapon selection
+        /// </summary>
+        public void OnWeaponCountChanged()
+        {
+            List<TMP_Dropdown.OptionData> options = new List<TMP_Dropdown.OptionData>();
+
+            for (int i = 0; i < value; i++)
+                options.Add(new TMP_Dropdown.OptionData("Weapon " + i));
+
+            weaponSelectionDropdown.options = options;
+            weaponSelectionDropdown.SetValueWithoutNotify(0);
+        }
+
+        /// <summary>
+        /// Called when the weapon dropdown changes - need to refresh weapon data
+        /// </summary>
+        /// <param name="val"></param>
+        public void OnWeaponSelectionChanged(int val)
+        {
+            for (int i = 0; i < individualWeaponSliders.Length; i++)
+                individualWeaponSliders[i].SetValueWithoutNotify(individualWeaponSliders[i].minValue);
+
+            for (int i = 0; i < individualWeaponDropdowns.Length; i++)
+                individualWeaponDropdowns[i].SetValueWithoutNotify(0);
         }
     }
 }
